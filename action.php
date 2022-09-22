1c2a0427255b0d4538fe35043bcc4c8734407451
Baimenov Timour <buchukiri@gmail.com>
2022-09-22 16:56:52 +0200

    add last modifs

--- action.php
+++ action.php
@@ -16,38 +16,38 @@
         ]);
     } 
 
-    elseif (isset($_GET["action"])&& isset($_GET["id_task"])&& ($_GET["action"]==="modify")) {
-        $query = $dbCo->prepare("UPDATE `task` SET `id_task`=:id_task,`description`=:description,`color`=:color,`priority`= :priority,`date_reminder`=:date_reminder,`done`=:done,`id_user`=:id_user WHERE id_task = :id_task;");
+    if (isset($_GET["action"])&& isset($_GET["id_task"])&& isset($_GET["description"]) && isset($_GET["color"]) && isset($_GET["priority"]) && isset($_GET["date_reminder"]) && isset($_GET["done"]) && isset($_GET["id_user"]) && ($_GET["action"]==="modify")) {
+        $query = $dbCo->prepare("UPDATE task SET description = :description,color = :color, priority = :priority, date_reminder = :date_reminder, done = :done, id_user = :id_user WHERE id_task = :id_task;");
         $query->execute([
             "id_task" => $_GET["id_task"],
             "description" => $_GET["description"],
-            "taskColor" => $_GET["task-color"],
-            "priority" => $_GET["description"],
-            "date_reminder" => $_GET["task-date-reminder"],
-            "done" => $_GET["task-done"],
-            "id_user" => $_GET["id-user"]
+            "color" => $_GET["color"],
+            "priority" => $_GET["priority"],
+            "date_reminder" => $_GET["date_reminder"],
+            "done" => $_GET["done"],
+            "id_user" => $_GET["id_user"]
         ]);
     } 
-    else {
-        $description = $_POST['description'];
-        $description = ucfirst($description);
-        $priority = $_POST['priority'];
-        $dateReminder = $_POST['task-date-reminder'];
-        $taskColor = $_POST['task-color'];
-        $done = $_POST['task-done'];
-        $idUser = $_POST['id-user'];
+    // else {
+    //     $description = $_POST['description'];
+    //     $description = ucfirst($description);
+    //     $priority = $_POST['priority'];
+    //     $dateReminder = $_POST['task-date-reminder'];
+    //     $taskColor = $_POST['task-color'];
+    //     $done = $_POST['task-done'];
+    //     $idUser = $_POST['id-user'];
         
-        $query = $dbCo->prepare("INSERT INTO `task` (`id_task`, `description`, `color`, `priority`, `date_reminder`, `done`, `id_user`) VALUES (NULL, :description, :taskColor, :priority, :date_reminder, :done, :id_user)");
-        $newColor = strtoupper(str_replace("#",NULL,$taskColor));
-        $query->execute([
-            "description" => $description,
-            "taskColor" => $newColor,
-            "priority" => $priority,
-            "date_reminder" => $dateReminder,
-            "done" => $done,
-            "id_user" => $idUser
-        ]);
-    }
+    //     $query = $dbCo->prepare("INSERT INTO `task` (`id_task`, `description`, `color`, `priority`, `date_reminder`, `done`, `id_user`) VALUES (NULL, :description, :taskColor, :priority, :date_reminder, :done, :id_user)");
+    //     $newColor = strtoupper(str_replace("#",NULL,$taskColor));
+    //     $query->execute([
+    //         "description" => $description,
+    //         "taskColor" => $newColor,
+    //         "priority" => $priority,
+    //         "date_reminder" => $dateReminder,
+    //         "done" => $done,
+    //         "id_user" => $idUser
+    //     ]);
+    // }
 
     
 
--- index.php
+++ index.php
@@ -7,28 +7,28 @@
         <form action="action.php" method="post" class="task-form">
           <div class="task-input">
             <label name="new-task" for="description">Enter New Task :</label>
-            <input type="text" name="description" class="description" value="<?=isset($_GET['modify']) ? $_GET['description'] : ""?>" required>
+            <input type="text" name="description" class="description" value="<?=isset($_POST['modify']) ? $_POST['description'] : ""?>" required>
           </div>
             <div class="tasks-configs">
               <div class="task-priority">
                 <label name="new-priority" for="priority">Priority :</label>
-                <input type="number" value="1" min="1" max="11" name="priority" value="<?=isset($_GET['modify']) ? $_GET['priority'] : ""?>" required>
+                <input type="number" value="1" min="1" max="11" name="priority" value="<?=isset($_POST['modify']) ? $_POST['priority'] : ""?>" required>
               </div>
                 <div class="task-date-reminder">
                 <label name="task-date-reminder" for="date_reminder">Date Reminder :</label>
-              <input type="date" name="task-date-reminder" value="<?=isset($_GET['modify']) ? $_GET['date_reminder'] : ""?>" required>
+              <input type="date" name="task-date-reminder" value="<?=isset($_POST['modify']) ? $_POST['date_reminder'] : ""?>" required>
               </div>
               <div class="task-Color">
                 <label for="color">Select color :</label>
-                <input type="color" id="favcolor" name="task-color" value="#ff0000" value="<?=isset($_GET['modify']) ? $_GET['color'] : ""?>" required>
+                <input type="color" id="favcolor" name="task-color" value="#ff0000" value="<?=isset($_POST['modify']) ? $_POST['color'] : ""?>" required>
               </div>
               <div class="task-done">
                 <label name="task-done" for="done">Situation :</label>
-                <input type="number" min="0" max="1" value="0" name="task-done" value="<?=isset($_GET['modify']) ? $_GET['done'] : ""?>" required>
+                <input type="number" min="0" max="1" value="0" name="task-done" value="<?=isset($_POST['modify']) ? $_POST['done'] : ""?>" required>
               </div>
               <div class="task-id-user">
                 <label name="id-user" for="id_user">ID user :</label>
-                <input type="number"  min="1" max="5" value="1" name="id-user" value="<?=isset($_GET['modify']) ? $_GET['id_user'] : ""?>" required>
+                <input type="number"  min="1" max="5" value="1" name="id-user" value="<?=isset($_POST['modify']) ? $_POST['id_user'] : ""?>" required>
               </div>
               </div>
           <div class="form-submit">
@@ -60,7 +60,7 @@
                 <p>".$task["done"]."</p>
                 <p>".$task["id_user"]."</p>
                 <li class=\"config-task\">
-                <a href=\"action.php?action=modify&id_task=".$task["id_task"]."\"><img class=\"pen\" src=\"img/pen.png\" alt=\"pen_button\"></a>
+                <a href=\"action.php?action=modify&id_task=".$task["id_task"]."&description=".$task["description"]."&priority=".$task["priority"]."&color=".$task["color"]."&done=".$task["done"]."&date_reminder=".$task["date_reminder"]."&id_user=".$task["id_user"]."\"><img class=\"pen\" src=\"img/pen.png\" alt=\"pen_button\"></a>
                 <a href=\"action.php?action=done&id_task=".$task["id_task"]."\"><img class=\"check\" src=\"img/check.png\" alt=\"check_button\"></a>
                 <a href=\"action.php?action=delete&id_task=".$task["id_task"]."\"><img class=\"delete\" src=\"img/delete.png\" alt=\"delete_button\"></a>
                 </li>
